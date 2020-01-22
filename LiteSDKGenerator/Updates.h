2371178cba902e137f54e6ab9036400e1998b8ba
COHRENCE <33120952+C0HERENCE@users.noreply.github.com>
2020-01-22 16:37:56 +0800

    updated to latest version

--- LiteSDKGenerator/Updates.h
+++ LiteSDKGenerator/Updates.h
@@ -8,37 +8,37 @@ namespace Updates
 	{
 	public:
 		// Main
-		static const uint64 GNames = 0x45e7420;
-		static const uint64 ObjObjects = 0x45e8a00;
+		static const uint64 GNames = 0x453B6F0;
+		static const uint64 ObjObjects = 0x46BCDB0;
 		//GNames
-		static const uint64 chunksize = 0x40f0;
-		static const uint64 static_gnames = 0x86AF8;
+		static const uint64 chunksize = 0x4030;
+		static const uint64 static_gnames = 0x11EA78;
 		// UObject
-		static const uint64 ulcass = 0x10;
-		static const uint64 internal_id = 0x20;
-		static const uint64 name = 0x18;
-		static const uint64 outer = 0x28;
+		static const uint64 ulcass = 0x20;
+		static const uint64 internal_id = 0x1c;
+		static const uint64 name = 0x14;
+		static const uint64 outer = 0x8;
 		// FName
 		static const uint64 number = 0x4;
 		static const uint64 comparison_id = 0x0;
 		// UField
-		static const uint64 next = 0x30;
+		static const uint64 next = 0x28;
 		// UEnum
-		static const uint64 enumNames = 0x48;
+		static const uint64 enumNames = 0x40;
 		// UStruct
-		static const uint64 superfield = 0x40;
-		static const uint64 property_size = 0x48;
-		static const uint64 children = 0x60;
+		static const uint64 superfield = 0x68;
+		static const uint64 property_size = 0x88;
+		static const uint64 children = 0x50;
 		// UFunction
-		static const uint64 func = 0x98;
-		static const uint64 function_flags = 0xA0;
+		static const uint64 func = 0x90;
+		static const uint64 function_flags = 0xB8;
 		// UProperty
-		static const uint64 array_dim = 0x38;
-		static const uint64 element_size = 0x3C;
-		static const uint64 offset = 0x4C;
-		static const uint64 property_flag = 0x40;
+		static const uint64 array_dim = 0x30;
+		static const uint64 element_size = 0x34;
+		static const uint64 offset = 0x44;
+		static const uint64 property_flag = 0x38;
 
-		static const uint64 uproperty_size = 0x78;
+		static const uint64 uproperty_size = 0x70;
 	};
 
 	class Dec
@@ -47,31 +47,30 @@ namespace Updates
 		// UObject
 		static uint64 objobjects(uint64 v7)
 		{
-			uint64 v392;
-			LODWORD(v392) = (v7 - 1524127531) ^ 0x95F5A15B;
-			HIDWORD(v392) = (HIDWORD(v7) - 301227509) ^ 0xEB85EBC5;
-			return v392;
+			uint64 add = ((v7 & 0x15151515 ^ v7 & 0xEAEAEAEA) - 1960050811) ^ 0x8B2BFF85;
+			HIDWORD(add) = 0x0000021e; // manual edit here.
+			return add;
 		}
 		static int32 internal_id(uint32 v248)
 		{
-			return __ROR4__(v248 ^ 0xF975671B, 5) ^ (__ROR4__(v248 ^ 0xF975671B, 5) << 16) ^ 0x6E9E74C0;;
+			return __ROL4__(v248 ^ 0x3F134169, 5) ^ (__ROL4__(v248 ^ 0x3F134169, 5) << 16) ^ 0x7A7E1103;
 		}
-		static uint64 uclass(uint64 v6)
+		static uint64 uclass(uint64 v14)
 		{
-			return __ROL8__(v6 ^ 0x28F17C97CE8EDCD2i64, 13) ^ (__ROL8__(v6 ^ 0x28F17C97CE8EDCD2i64, 13) << 32) ^ 0xCBE32EC1DB22E7ADui64;
+			return __ROL8__(v14 ^ 0x6D93A24294179303i64, 19) ^ (__ROL8__(v14 ^ 0x6D93A24294179303i64, 19) << 32) ^ 0xE87A094B09D169B2ui64;
 		}
-		static uint64 outer(uint64 v18)
+		static uint64 outer(uint64 v30)
 		{
-			return __ROR8__(v18 ^ 0x9E64E044D0CC1596ui64, 6) ^ (__ROR8__(v18 ^ 0x9E64E044D0CC1596ui64, 6) << 32) ^ 0xADA45B881BD3BC1Fui64;
+			return __ROR8__(v30 ^ 0x6215B3BA387150AAi64, 28) ^ (__ROR8__(v30 ^ 0x6215B3BA387150AAi64, 28) << 32) ^ 0x479B8E128B695D5Ai64;
 		}
 		//FName
-		static int32 comparison_id(uint32 v10)
+		static int32 comparison_id(uint32 v15)
 		{
-			return __ROL4__(v10 ^ 0x6F08BDBC, 12) ^ (__ROL4__(v10 ^ 0x6F08BDBC, 12) << 16) ^ 0x5EA13260;
+			return __ROR4__(v15 ^ 0x7766ED5E, 14) ^ (__ROR4__(v15 ^ 0x7766ED5E, 14) << 16) ^ 0xF159012D;
 		}
 		static int32 number(uint32 v6)
 		{
-			return __ROL4__(v6 ^ 0x216EDEF7, 8) ^ (__ROL4__(v6 ^ 0x216EDEF7, 8) << 16) ^ 0x20DF5F52;
+			return __ROR4__(v6 ^ 0x12DF074, 4) ^ (__ROR4__(v6 ^ 0x12DF074, 4) << 16) ^ 0x9A387766;
 		}
 	};
 }
